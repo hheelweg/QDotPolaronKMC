8f243b25346e1fe022a8602edae6609058089fb0
Henrik Heelweg <124303636+hheelweg@users.noreply.github.com>
2025-08-07 17:10:31 -0400

    some tests

--- src/hamiltonian_box.py
+++ src/hamiltonian_box.py
@@ -90,8 +90,8 @@ class SpecDens():
 
             hilb = np.imag(hilbert(re_vals))
             # Interpolators
-            re_interp = interp1d(omega_grid, re_vals, kind='cubic', bounds_error=False, fill_value=0.0)
-            hilb_interp = interp1d(omega_grid, hilb, kind='cubic', bounds_error=False, fill_value=0.0)
+            self.re_interp = interp1d(omega_grid, re_vals, kind='cubic', bounds_error=False, fill_value=0.0)
+            self.hilb_interp = interp1d(omega_grid, hilb, kind='cubic', bounds_error=False, fill_value=0.0)
 
     
         if self.bath_method == 'first-order':
@@ -108,25 +108,7 @@ class SpecDens():
         Jw = (self.lamda/(2*self.omega_c**3))*w**3*np.exp(-w/self.omega_c)
         return Jw*(omega >= 0) - Jw*(omega < 0)
     
-    def gauss_sum(self, omega):
-        """Evaluage a spectral density that approximates the sum of several gaussian peaks"""
-        if type(omega) == float or type(omega) == int or type(omega) == np.float64:
-            w = np.array([np.abs(omega)])
-        else:
-            w = np.abs(omega)
-        spec = np.zeros(np.shape(w))
-        for i in np.arange(len(self.heights)):
-            spec += self.heights[i] * np.exp(-0.5 * (w - self.peak_nrgs[i])**2/(self.sds[i]**2))
-        # modify behavior near 0 to avoid divergent behavior
-        # cutoff_nrg = min(self.peak_nrgs/10)
-        cutoff_nrg = 0.001
-        zero_taper = np.zeros(np.shape(w))
-        taper_inds = np.nonzero(np.logical_and(w > cutoff_nrg,w < 2*cutoff_nrg))
-        zero_taper[taper_inds] = (w[taper_inds] - 0.001)/0.001
-        zero_taper[np.nonzero(w >= 2 * cutoff_nrg)] = 1
-        spec *= zero_taper
-        return spec             
-        
+            
     # compute phi based on Eq. (17)
     def phi(self, tau):
         
@@ -192,151 +174,9 @@ class SpecDens():
                 bathCorrFT_imag2[i]=integrate.quad(integrandFT_real, lowLim, uppLim, limit=200, weight='sin', wvar=omega[i], limlst = 200)[0]
 
             return bathCorrFT_real1+bathCorrFT_real2+1j*bathCorrFT_imag1-1j*bathCorrFT_imag2
-    """
-    # perform half-sided Fourier trasnform of bath correlation function based on Eq. (15)
-    # old version, doesn't work with vectorized inputs
-    def bathCorrFT(self, omega, lamda, kappa):
+
+
         
-        if lamda==0:
-            return 0
-        else:
-    
-            def integrandFT_real(tau):
-                return kappa**2*np.real(np.exp(-lamda*self.Phi(tau))-1)
-            def integrandFT_imag(tau):
-                return kappa**2*np.imag(np.exp(-lamda*self.Phi(tau))-1)
-            
-            # perform half-sided Fourier transform (real/imaginary parts separately)
-            uppLim=np.inf
-            lowLim=1E-14
-            bathCorrFT_real1=integrate.quad(integrandFT_real, lowLim, uppLim, limit=200, weight='cos', wvar=omega)
-            bathCorrFT_real2=integrate.quad(integrandFT_imag, lowLim, uppLim, limit=200, weight='sin', wvar=omega)
-            bathCorrFT_imag1=integrate.quad(integrandFT_imag, lowLim, uppLim, limit=200, weight='cos', wvar=omega)
-            bathCorrFT_imag2=integrate.quad(integrandFT_real, lowLim, uppLim, limit=200, weight='sin', wvar=omega)
-    
-            return bathCorrFT_real1[0]+bathCorrFT_real2[0]+1j*bathCorrFT_imag1[0]-1j*bathCorrFT_imag2[0]
-    """
-    
-    def getPhiFit(self):
-        # determine tau_max for which asolute value of phi exceeds cutoff
-        cutoff = 1E-5 
-        taus_range = np.arange(0.1, 400, 2)
-        integrals_range = [self.phi(tau).real[0] for tau in taus_range]
-        cutoff_idx = np.min(np.where(np.flip(np.asarray(integrals_range)) >= cutoff))
-        taus_max = taus_range[len(taus_range)-1-cutoff_idx]
-        
-        # parameters for fitting (might have to adjust these from case to case)
-        self.taus = np.linspace(0.1, taus_max, 2000)
-        polyDegree = 20  # degree polynomial for fit
-        
-        # evaluate exact integral for all taus in tau
-        self.integral_real=np.array([self.phi(tau).real[0] for tau in self.taus])
-        self.integral_imag=np.array([self.phi(tau).imag[0] for tau in self.taus])
-        
-        # get inflection points for real and imaginary part of phi:
-        idx_real, self.cutoff_real = get_inflection(self.taus, self.integral_real)
-        idx_imag, self.cutoff_imag = get_inflection(self.taus, self.integral_imag)
-        
-        # until cutoff: perfrom polynomial fit
-        def polynomialFit(taus, phis):
-            params = np.polyfit(taus, phis, polyDegree)
-            return params
-        
-        self.polyParams_real = polynomialFit(self.taus[:idx_real], self.integral_real[:idx_real])
-        self.polyParams_imag = polynomialFit(self.taus[:idx_imag], self.integral_imag[:idx_imag])
-        
-        # after cutoff: perform biexponential decay fit
-        def biexpFit(taus, phis):
-            bounds = (-np.inf, np.inf)
-            initial = [-1, 1,1, 1, 1,1]
-            params, pcov = optimize.curve_fit(biexpDecay, xdata=taus, ydata=phis,
-                                            p0= initial, bounds=bounds, maxfev=10000, method="trf") 
-            return params
-        
-        self.expParam_real = biexpFit(self.taus[idx_real:]-self.cutoff_real, self.integral_real[idx_real:])
-        self.expParam_imag = biexpFit(self.taus[idx_imag:]-self.cutoff_imag, self.integral_imag[idx_imag:])
-        
-        
-    def getChebyPhiFit(self):
-        degree = 100
-        self.chebyphi_real = np.polynomial.chebyshev.Chebyshev.interpolate(lambda tau: np.real(self.phi(tau)), degree, [0, self.cheby_tau_cutoff])
-        self.chebyphi_im = np.polynomial.chebyshev.Chebyshev.interpolate(lambda tau: np.imag(self.phi(tau)), degree, [0, self.cheby_tau_cutoff])
-        
-        # after cutoff: perform biexponential decay fit
-        def biexpFit(taus, phis):
-            bounds = (-np.inf, np.inf)
-            initial = [phis[0], 0,0, -phis[0], 0,0]
-            params, pcov = optimize.curve_fit(biexpDecay, xdata=taus, ydata=phis,
-                                            p0= initial, bounds=bounds, maxfev=10000, method="trf") 
-            return params
-        self.biexpFitTaus = np.linspace(self.cheby_tau_cutoff, self.cheby_tau_cutoff * 2, 100)
-        self.expParam_real = biexpFit(self.biexpFitTaus - self.cheby_tau_cutoff, np.real(self.phi(self.biexpFitTaus)))
-        self.expParam_imag = biexpFit(self.biexpFitTaus - self.cheby_tau_cutoff, np.imag(self.phi(self.biexpFitTaus)))
-    
-    def getBathCorrFTFit(self, max_energy_diff):
-        degree = 25
-        self.chebyk_real = []
-        # note: calculates bath corr FT divided by kappa^2
-        # kappa^2 is added later for calculating final term
-        for lamda in [-2, -1, 0, 1, 2]:
-            self.chebyk_real.append(np.polynomial.chebyshev.Chebyshev.interpolate(lambda omega: np.real(self.bathCorrFT(omega, lamda, 1)), degree, [-max_energy_diff, max_energy_diff]))
-    
-    def qualityFit(self):
-        # compute the quality of the fitted function for phi by computing the mean least squared deviations
-        integral_fit = np.array([self.PhiFit(tau) for tau in self.taus])
-        # real part and imaginary part
-        mean_sq_error = [np.mean(np.square(self.integral_real-integral_fit.real)), 
-                         np.mean(np.square(self.integral_imag-integral_fit.imag))]
-        # raise warning if prespecified fit_tolerance is exceeded
-        fit_tolerance = 1E-4
-        if any(i >= fit_tolerance for i in mean_sq_error):
-            warnings.warn("fit tolerance {} exceeded! Please check fit parameters. Results might be wrong!".format(fit_tolerance),
-                          stacklevel = 2)
-        
-    def PhiFit(self, tau):
-        # real part of phi
-        if tau <= self.cutoff_real:
-            phi_real = np.poly1d(self.polyParams_real)(tau)
-        else:
-            phi_real = biexpDecay(tau-self.cutoff_real, *self.expParam_real)
-        # imaginary part of phi
-        if tau <= self.cutoff_imag:
-            phi_imag = np.poly1d(self.polyParams_imag)(tau)
-        else:
-            phi_imag = biexpDecay(tau-self.cutoff_imag, *self.expParam_imag)
-        
-        return phi_real + 1j*phi_imag
-    
-    def chebyPhiFit(self, tau):
-        if tau <= self.cheby_tau_cutoff:
-            return self.chebyphi_real(tau) + 1j * self.chebyphi_im(tau)
-        else:
-            return biexpDecay(tau-self.cheby_tau_cutoff, *self.expParam_real) + 1j * biexpDecay(tau-self.cheby_tau_cutoff, *self.expParam_imag)
-    
-    # only returns real portion
-    def bathCorrFTFitReal(self, omega, lamda, kappa):
-        return kappa**2 * self.chebyk_real[int(lamda) + 2](omega)
-    
-    def plotFit(self):
-        import matplotlib.pyplot as plt
-        taus = np.linspace(0, self.cheby_tau_cutoff * 2, 400)
-        exactphi = self.phi(taus)
-        realchebyphi = np.zeros(400)
-        imagchebyphi = np.zeros(400)
-        for i in np.arange(400):
-            realchebyphi[i] = np.real(self.chebyPhiFit(taus[i]))
-            imagchebyphi[i] = np.imag(self.chebyPhiFit(taus[i]))
-        plt.plot(taus, np.real(exactphi))
-        plt.plot(taus, realchebyphi)
-        plt.show()
-        plt.plot(taus, np.real(exactphi) - realchebyphi)
-        plt.show()
-        plt.plot(taus, np.imag(exactphi))
-        plt.plot(taus, imagchebyphi)
-        plt.show()
-        plt.plot(taus, np.imag(exactphi) - imagchebyphi)
-        return
-    
     # first order approximation to the bath correlation function in Eq. (16)
     def firstOrderFT(self, omega, lamda, kappa):
         
@@ -357,29 +197,6 @@ class SpecDens():
         ppv = -ppv[0]
         return -kappa**2*lamda*(re_bath_corr(omega) + (1j/np.pi)*ppv)
 
-
-
-
-# auxiliary functions for method = 'fit'
-def get_inflection(x, y):
-    """
-    compute rightmost inflection points for y
-    ----------
-    x : 1D-array 
-    y : 1D-array (same length as x)
-
-    Returns
-    -------
-    rightmost inflection point as IDX and as x value
-    """
-    second_deriv = np.gradient(np.gradient(y))
-    inflectionsIDX = np.where(np.diff(np.sign(second_deriv)))[0] # returns all inflection points as indices in y
-    inflection = x[inflectionsIDX[len(inflectionsIDX)-1]]
-    return inflectionsIDX[len(inflectionsIDX)-1], inflection
-
-
-def biexpDecay(x, a,b,c,d,e,f):
-    return a * np.exp(-b*(x-c))-d * np.exp(-e*(x-f))
 
 
 
