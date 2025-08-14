87da4465655887afa5e6bd65be157e91b92f5cbe
Henrik Heelweg <hheelweg@mit.edu>
2025-08-14 15:19:24 -0400

    some tests

--- src/utils.py
+++ src/utils.py
@@ -1,6 +1,6 @@
 import numpy as np
 import scipy.linalg as la
-import matplotlib.pyplot as plt
+
 
 """
 This file contains some helper functions that will be called
@@ -9,7 +9,7 @@ This file contains some helper functions that will be called
 
 # diagonalize Hamiltonian
 # NOTE : might want to make this more efficient with GPU/torch etc.
-def diagonalize(H,S=None):
+def diagonalize(H, S=None):
     """
     Diagonalize a real, symmetrix matrix and return sorted results.
     
@@ -27,44 +27,5 @@ def diagonalize(H,S=None):
 
     return E,C
 
-
-
-def draw_circle(center, radius):
-    # feeds circle-center (np.array), and radius
-    return plt.Circle(tuple(center), radius, color = 'grey', alpha = 0.1)
-
-
-def scatterPoints(points, color, s, label):
-
-    # dimension of qd_lattice
-    dim = len(points[0])
-
-    if dim == 2:
-        plt.scatter(points.T[0], points.T[1], color = color, s = s, label = label)
-    elif dim == 1:
-        plt.scatter(points.T[0], np.ones_like(points.T[0]), color = color, s = s, label = label)
-
-
-# TODO : only implemented for 2D, need to check dimension and adjust accordingly
-def plot_lattice(points, qd_lattice, label = 'points', periodic = False):
-
-    # get dimension
-    dim = len(qd_lattice[0])
-
-    # if we use periodic boundary conditions for the plottings
-    if periodic:
-        max_length = np.max(qd_lattice)
-        points = np.mod(points, max_length)
-
-    # plot points 
-    #plt.scatter(points.T[0], points.T[1], color = 'C01', s = 5, label = label)
-    scatterPoints(points, color = 'C01', s = 5, label = label)
-
-    # plot qd_lattice
-    #plt.scatter(qd_lattice.T[0], qd_lattice.T[1], color = 'k', s = 2, label = 'QD lattice')
-    scatterPoints(qd_lattice, color = 'k', s = 2, label = 'QD lattice')
-    if dim == 2:
-        plt.gca().set_aspect('equal', adjustable='box')
-    plt.axis('off')
 
 
