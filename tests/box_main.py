7ee4e47306b9c9076f39e132c922cfb1151b6a0f
Henrik Heelweg <hheelweg@mit.edu>
2025-08-15 16:42:04 -0400

    some tests

--- tests/box_main.py
+++ tests/box_main.py
@@ -32,7 +32,7 @@ def main():
     spec_density = 'cubic-exp'                  # bath spectral density
 
     # PTRE and KMC related parameters
-    r_hop = 9                                   # hopping radius (see Kassal) (in units of lattice spacing)
+    r_hop = 12                                   # hopping radius (see Kassal) (in units of lattice spacing)
     r_ove = 14                                   # overlap radius (see Kassal) (in units of lattice spacing)
     
     ntrajs = 50                                 # number of trajectories to compute MSDs over
