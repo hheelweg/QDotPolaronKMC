4d3f1eac2bf4e16bd812fcf0b4d25656c4c69ea5
Henrik Heelweg <hheelweg@mit.edu>
2025-08-18 15:22:43 -0400

    some updates

--- tests/test_conv.py
+++ tests/test_conv.py
@@ -36,7 +36,7 @@ def main():
     r_ove = 5                                   # overlap radius (see Kassal) (in units of lattice spacing)
     
     ntrajs = 20                                 # number of trajectories to compute MSDs over
-    nrealizations = 1                           # number of disorder realizations (i.e. number of time we initialize a new QD lattice)
+    nrealizations = 8                           # number of disorder realizations (i.e. number of time we initialize a new QD lattice)
 
     t_final = 5                                # final time for each trajectory (units?)
     #-------------------------------------------------------------------------
