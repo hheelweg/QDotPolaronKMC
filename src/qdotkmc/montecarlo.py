0833e04c9fc18845430081ef696840fe7136a9ff
Henrik Heelweg <hheelweg@mit.edu>
2025-08-19 18:12:55 -0400

    some tests

--- src/qdotkmc/montecarlo.py
+++ src/qdotkmc/montecarlo.py
@@ -71,7 +71,7 @@ class KMCRunner():
         rates_criterion_per_center, rates_criterion = [], None
         for start_idx in start_sites:
 
-            rates, final_sites, _ = self._make_kmatrix_boxNEW(start_idx,
+            rates, final_sites, _ = self._make_kmatrix_boxNEW(qd_lattice, start_idx,
                                                               theta_sites=0.02,
                                                               theta_pol=0.1
                                                               )
@@ -252,7 +252,7 @@ class KMCRunner():
         qd_lattice.redfield.theta_pol, qd_lattice.redfield.theta_sites = theta_pol, theta_sites
 
         # (1) select sites and polarons that ''matter'' for computing the rates
-        site_g, pol_g = qd_lattice.redfield.select_sites_and_polarons(qd_lattice, 
+        site_g, pol_g = qd_lattice.redfield.select_sites_and_polarons(
                                                                       center_global=center_global, 
                                                                       theta_sites= qd_lattice.redfield.theta_sites, 
                                                                       theta_pol = qd_lattice.redfield.theta_pol, 
