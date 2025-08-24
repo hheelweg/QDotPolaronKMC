000994b632de141d8cc0781b82ddcf7cbc4a9917
Henrik Heelweg <hheelweg@mit.edu>
2025-08-24 14:36:16 -0400

    accelerate GPU mode

--- src/qdotkmc/redfield.py
+++ src/qdotkmc/redfield.py
@@ -28,7 +28,6 @@ def _gpu_available() -> bool:
         return False
 
 
-
 class Redfield():
 
     def __init__(self, hamiltonian, polaron_locations, site_locations, kappa,
@@ -525,6 +524,7 @@ class Redfield():
         t2 = time.time()
         if self.use_gpu:
             # run on GPU
+            print('run GPU')
             gamma_plus = _build_gamma_plus_gpu(J, J2, Up, u0, bath_map, use_c64=self.gpu_use_c64)
         else:
             # run on CPU
