6324ca8ebc900ab31c60b4b4bc77130541353bd3
Henrik Heelweg <hheelweg@mit.edu>
2025-08-25 13:40:40 -0400

    added GPU/CPU backend

--- src/qdotkmc/redfield.py
+++ src/qdotkmc/redfield.py
@@ -57,6 +57,7 @@ class Redfield():
         # enable GPU only if user asked and a device exists
         env_wants_gpu = (os.getenv("QDOT_USE_GPU", "0") == "1")
         self.use_gpu = bool(env_wants_gpu and _gpu_available())
+        print('use GPU:', self.use_gpu)
         # allow memory allocator if GPU exists
         if self.use_gpu:
             pool = cp.cuda.MemoryPool()
@@ -706,12 +707,20 @@ class Redfield():
 
         # (4) build 𝛾_+(𝜈')
         t2 = time.time()
-        if self.use_gpu:
-            # run on GPU
-            gamma_plus = Redfield._build_gamma_plus_gpu(J, J2, Up, u0, bath_map, use_c64=self.gpu_use_c64)
-        else:
-            # run on CPU
-            gamma_plus = Redfield._build_gamma_plus_cpu(J, J2, Up, u0, bath_map)  
+        # # if loop GPU/CPU switch
+        # if self.use_gpu:
+        #     # run on GPU
+        #     gamma_plus = Redfield._build_gamma_plus_gpu(J, J2, Up, u0, bath_map, use_c64=self.gpu_use_c64)
+        # else:
+        #     # run on CPU
+        #     gamma_plus = Redfield._build_gamma_plus_cpu(J, J2, Up, u0, bath_map)  
+        gamma_plus = Redfield._build_gamma_plus_backend(
+                    J, J2, Up, u0, bath_map,
+                    prefer_gpu=self.use_gpu,    # your flag
+                    use_c64=self.gpu_use_c64    # your flag
+                )
+
+
         if time_verbose:
             print('time(gamma accumulation)', time.time() - t2, flush=True)
 
