553da745633ac5e87aebeb9a87c142ee22b2e620
Henrik Heelweg <hheelweg@mit.edu>
2025-08-26 14:38:43 -0400

    some diagonalization tests

--- src/qdotkmc/utils.py
+++ src/qdotkmc/utils.py
@@ -8,7 +8,7 @@ from . import lattice
 # diagonalize Hamiltonian
 # NOTE : might want to make this more efficient with GPU/torch etc.
 def diagonalize(H, S=None, 
-                cpu_threads: int = 16,
+                cpu_threads: int = 8,
                 cpu_driver: str = "evr",
                 uplo: str = "L"):
     """
