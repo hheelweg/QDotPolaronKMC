889ce2f9e7693c35c32fba49d8c782977372c853
Henrik Heelweg <hheelweg@mit.edu>
2025-08-26 14:27:27 -0400

    some diagonalization tests

--- src/qdotkmc/lattice.py
+++ src/qdotkmc/lattice.py
@@ -171,7 +171,11 @@ class QDLattice():
 
         # (2) keep original diagonalization routine
         # NOTE : can we improve this function somehow? (maybe torch/GPU/cupy?)
+        import time
+        start = time.time()
         self.eignrgs, self.eigstates = utils.diagonalize(self.hamil)
+        end = time.time()
+        print(f"time taken for diagonalization: {end-start:.4f}")
 
         # (3) polaron positions 
         if periodic:
