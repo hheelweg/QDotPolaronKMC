d91928991127f45a541c34835a09879a20e37353
teeshee <tsheeh@mit.edu>
2025-08-28 11:29:33 -0400

    Update main_bashversion_spat_disorder.py

--- legacy/main_bashversion_spat_disorder.py
+++ legacy/main_bashversion_spat_disorder.py
@@ -4,8 +4,7 @@ import matplotlib.pyplot as plt
 from mpl_toolkits.axes_grid1 import make_axes_locatable
 from mpl_toolkits.mplot3d import Axes3D  
 import numpy as np
-import src.montecarlo as mc
-import src.utils as utils
+import qdotkmc
 import math
 import matplotlib.pyplot as plt
 from multiprocessing import Pool, cpu_count
@@ -16,11 +15,10 @@ def main():
     N = int(sys.argv[1])
     ndims = np.array([1]).astype(int)
     J_cs = np.array([0.3, 3, 30])
-    inhomog_sds = np.array([0, 0.001, 0.01])
+    inhomog_sds = np.array([0.001, 0.003, 0.01, 0.03])
     reorg_nrgs = np.array([0.03, 0.1, 0.3])
     w_cs = np.array([0.01, 0.03, 0.1])
-    temps = np.linspace(1, 1000, 20)
-    rel_spatial_disorders = np.array([0, 0.1, 0.5])
+    # rel_spatial_disorders = np.array([0, 0.1, 0.5])
     # r_hops = np.array([4,5]).astype(int)
     # r_oves = np.array([5.5, 6.5])
     
@@ -39,9 +37,8 @@ def main():
             for j in range(len(inhomog_sds)):
                 for k in range(len(reorg_nrgs)):
                     for l in range(len(w_cs)):
-                        for m in range(len(rel_spatial_disorders)):
-                            params = [ndims[h], J_cs[i], inhomog_sds[j], reorg_nrgs[k], w_cs[l], rel_spatial_disorders[m]]
-                            args.append((params, N))
+                        params = [ndims[h], J_cs[i], inhomog_sds[j], reorg_nrgs[k], w_cs[l]]
+                        args.append((params, N))
     
     with Pool(numPara) as pool:
         results = pool.starmap(run_sim, args)
@@ -50,82 +47,73 @@ def main():
 
 def run_sim(args, N):
     print("run started", flush = True)
-    ndim = args[0]
-    J_c = args[1]
-    inhomog_sd = args[2]
-    reorg_nrg = args[3]
-    w_c = args[4]
-    temps = np.linspace(1, 1000, 20)
-    rel_spatial_disorder = args[5]
+    temps = np.linspace(1, 1000, 50)
+    # rel_spatial_disorder = args[5]
     
-    
-    # ndim = 1                                    # number of dimensions
-    N = 200                                      # number of QDs in each dimension
-    nc_edgelength = 8                           # length of each QD (units?)
-    ligand_length = 1                           # length of ligands on QD (units?)
+    # NOTE : a lot of the input parameters (especially the ones that are not used regularly)
+    # have been moved as defaults to .config dataclasses. 
 
-    # seed for randomness of Hamiltonian (if None, then Hamiltonian is randomly drawn for every instance of the class)
-    seed = None
+    # ---- QDLattice gometry ------
+    ndim = args[0]                                    # number of dimensions
+    # N = 50                                      # number of QDs in each dimension
 
-    # Hamiltonian and bath related parameters
-    # reorg_nrg = 0.01                            # reorganization energy (units?)
-    # w_c = 0.1                                   # cutoff frequency (units?) 
-    # J_c = 30                                    # J_c (units?)
-    # inhomog_sd = 0.002                           # inhomogenous broadening (units?)
+    # ---- system parameters ------
+    inhomog_sd = args[2]                          # inhomogenous broadening (units?)
     nrg_center = 2.0                            # mean site energy (units ?)
-    # rel_spatial_disorder = 0.0                  # relative spatial disorder
-    dipolegen = 'random'                        # dipole generation procedure
+    J_c = args[1]                                    # J_c (units?)
+
+    # ----- bath parameters -------
+    w_c = args[4]                                   # cutoff frequency (units?)
     # temp = 200                                  # temperature (K)
-    spec_density = 'cubic-exp'                  # bath spectral density
+    reorg_nrg = args[3]                            # reorganization energy (units?)
 
-    # PTRE and KMC related parameters
-    numtrials = 1                               # number of trials to average over (here: 3)
-    method = 'first-order'                      # method for computing bath integrals 
-    r_hop = 5                                   # hopping radius (see Kassal) (in units of lattice spacing)
-    r_ove = 6.5
-    r_box = math.ceil(min(r_hop, r_ove))        
-                                # overlap radius (see Kassal) (in units of lattice spacing)
+    # ---- KMC parameters ---------
+    ntrajs = 5000                                 # number of trajectories to compute MSDs over
+    nrealizations = 50                           # number of disorder realizations (i.e. number of time we initialize a new QD lattice)
+    t_final = 10
 
-    ntrajs = 1000                                 # number of trajectories to compute MSDs over
-    t_final = 3                                   # final time for each trajectory (units?)
-    #-------------------------------------------------------------------------
+    rates_by = "weight"                         # select mode/strategy for rates comutation
+    # NOTE : as soon as we pick "radius" or "weight" we confine ourselves ro r_hop/r_ove or theta_site/theta_pol
+    # here we leave both 
 
-    # lattice spacing
-    spacing = nc_edgelength + 2 * ligand_length
+    # (2) for "weight"
+    theta_site = 0.01
+    theta_pol = 0.01
 
-    # specify the bath
-    spectrum = [spec_density, reorg_nrg, w_c, method]
 
+    # define dataclasses
+    geom = qdotkmc.config.GeometryConfig(dims = ndim, N = N)
+    dis  = qdotkmc.config.DisorderConfig(nrg_center = nrg_center, inhomog_sd = inhomog_sd, J_c = J_c)
+    run  = qdotkmc.config.RunConfig(ntrajs = ntrajs, nrealizations = nrealizations,
+                                    rates_by = rates_by, 
+                                    theta_site = theta_site, theta_pol = theta_pol, 
+                                    t_final = t_final
+                                    )
+    
+    # -------------------------------------------------------------------------
+    # print('diffusivity ', diff1, diff2)
+    # print('diffusivity error', sigma_D1, sigma_D2)
+    
     for temp in temps:
-        # greate instance of MC class to run KMC simulation
-        kmc_setup = mc.KMCRunner(ndim, N, spacing, nrg_center, inhomog_sd, dipolegen, seed, rel_spatial_disorder,
-                                    J_c, spectrum, temp, ntrajs, r_hop, r_ove, r_box)
+        bath_cfg = qdotkmc.config.BathConfig(temp = temp, w_c=w_c, reorg_nrg=reorg_nrg)
         
-        # perform a KMC simulation
-        times, msds = kmc_setup.NEW_simulate_kmc(t_final)
-        
-        diff, diff_err = kmc_setup.get_diffusivity_hh(msds, times, ndim)
-        
-        filenamebase = ("J_c_%f,sigma_i_%f,reorg_nrg_%f,w_c_%f,spat_disorder_%f")%(J_c, inhomog_sd, reorg_nrg, w_c, rel_spatial_disorder)
-        results_txt = open("results_" + filenamebase + ".txt", "a")
-        results_txt.write(str(diff) + "," + str(diff_err) + "\n")
-        results_txt.close()
-        
-        ipr = kmc_setup.get_ipr()
-        ipr_txt = open("ipr_" + filenamebase + ".txt", "a")
-        ipr_txt.write(str(ipr) + ",")
-        ipr_txt.close()
+        # set up KMC simulation
+        kmc = qdotkmc.montecarlo.KMCRunner(geom, dis, bath_cfg, run)
+        # perform KMC simulation (automatically switches parallel/serial based on max_workers)
+        times, msds = kmc._simulate_kmc()
 
-    
-    # without taking into account units:
-    # print('diffusivity ', diff)
-    # print('diffusivity error', diff_err)
-    
-    # test plot of the msds
-    # plt.xlabel(r'$t$')
-    # plt.ylabel('MSD')
-    # plt.plot(times, msds)
-    # plt.show()
+        # export msds as .csv file for inspection
+        qdotkmc.utils.export_msds(times, msds)
+        
+        # get noise-averaged (pooled) trajectory MSD
+        msds_mean = np.mean(msds, axis = 0)
+        
+        diff1, sigma_D1 = qdotkmc.utils.get_diffusivity(msds_mean, times, ndim)
+        
+        filenamebase = ("J_c_%f,sigma_i_%f,reorg_nrg_%f,w_c_%f")%(J_c, inhomog_sd, reorg_nrg, w_c)
+        results_txt = open("results_" + filenamebase + ".txt", "a")
+        results_txt.write(str(diff1) + "," + str(sigma_D1) + "\n")
+        results_txt.close()
 
 
 if __name__ == '__main__':
