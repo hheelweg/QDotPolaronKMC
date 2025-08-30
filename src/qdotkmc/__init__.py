2a9d8b72e2057979ee60a1d67e2f154638ff5fb8
Henrik Heelweg <hheelweg@mit.edu>
2025-08-30 13:51:33 -0400

    updated __init__

--- src/qdotkmc/__init__.py
+++ src/qdotkmc/__init__.py
@@ -9,11 +9,14 @@ __version__ = "1.0.2"
 
 
 """
-Version: 1.0.2 
-Date: 2025-08-22
+Version: 1.0.3 
+Date: 2025-08-30
 
 Notes:
 -   contains weight-based strategy to compute Redfield rates in a computationally efficient way
 -   contains convergence module to automatically enable tuning of hyperparamters θ_pol and θ_site
     to optimal values before running KMC calculations
+-   contains automatic GPU/CPU switch and allows running both convergence algorithms and rate computations
+    on GPU resources.
+-   ...
 """
