498ae588db7f598348e6228ce1c1bc3726c0d18d
teeshee <tsheeh@mit.edu>
2025-10-17 16:33:08 -0400

    Update utils.py

--- src/qdotkmc/utils.py
+++ src/qdotkmc/utils.py
@@ -314,7 +314,7 @@ def get_msd_array(msds, times, t_final, n_points = 1000):
     for i in np.arange(n_trajs):
         time_matrix_even[:, i] = t_list
         for j in np.arange(n_points):
-            index_of_time = np.searchsorted(times[:, i], t_list[j])-1
+            index_of_time = np.searchsorted(times[i], t_list[j])-1
             index_of_time = max(0, index_of_time)
             msds_matrix_even = msds[index_of_time, i]
     return msds_matrix_even, time_matrix_even
