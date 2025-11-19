d1a7aeaf84493fb68ba077f5582a2b6fbe3e4ed2
Henrik Heelweg <hheelweg@mit.edu>
2025-11-19 14:50:38 -0500

    implement different bath spec dens

--- src/qdotkmc/hamiltonian.py
+++ src/qdotkmc/hamiltonian.py
@@ -242,14 +242,14 @@ class SpecDens:
             self.alpha = spec_dens_list[1]
             self.omega_c = spec_dens_list[2]
             self.J = self.ohmic_exp
-            self.low_freq_cutoff = self.omega_c / 100.0
+            self.low_freq_cutoff = self.omega_c / 50.0
             self.omega_inf = 40 * self.omega_c
 
         elif sd_type == "drude-lorentz":
             self.lamda = spec_dens_list[1]
             self.omega_c = spec_dens_list[2]
             self.J = self.drude_lorentz
-            self.low_freq_cutoff = self.omega_c / 100.0
+            self.low_freq_cutoff = self.omega_c / 50.0
             self.omega_inf = 40 * self.omega_c
         
         else:
