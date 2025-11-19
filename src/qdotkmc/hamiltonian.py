2c1dbed64e7435db527406f4a7ada2f74fdbee78
Henrik Heelweg <hheelweg@mit.edu>
2025-11-19 11:20:02 -0500

    implement different bath spec dens

--- src/qdotkmc/hamiltonian.py
+++ src/qdotkmc/hamiltonian.py
@@ -156,7 +156,6 @@ class SpecDens:
         sd_type = spec_dens_list[0]
         self.beta = 1.0 / kT
         self.spectrum = spec_dens_list
-        print('init SpecDens')
 
         if sd_type == 'cubic-exp':
             self.lamda = spec_dens_list[1]
